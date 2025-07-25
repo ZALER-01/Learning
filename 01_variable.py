95928e524ed2593022a5422267ea1d42061b67a5
Your Name <myself.rituraj@gmail.com>
2025-07-26 01:18:05 +0530

    Initial commit

--- 01_variable.py
+++ 01_variable.py
@@ -1,3 +1,12 @@
 a = "zaler"
 b= 909
 c= 909.888
+d = None
+e = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
---
+++ 02_operator.py
@@ -0,0 +1,13 @@
+a=3 
+b=4
+
+#Arthematic Operator
+print("The value of 3 + 4 is " ,a+b)
+
+#Assignment Operator 
+
+# a+=4
+# print(a)
+# a-=2
+# print(a)
+# a*=6
