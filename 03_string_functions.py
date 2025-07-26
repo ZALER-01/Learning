a08e5438618e6db273f2e035ca9f0855efa84f94
Your Name <myself.rituraj@gmail.com>
2025-07-26 23:42:22 +0530

    add new feature

--- 03_string_functions.py
+++ 03_string_functions.py
@@ -1,28 +1,52 @@
-story = " Once uopn a time Ritraj started Learnig python using code with Harry lets see how it goes "
+# story = " Once uopn a time Ritraj started Learnig python using code with Harry lets see how it goes "
     
-# Print the length of the story string
-print(len(story))  # Output: 77
+# # Print the length of the story string
+# print(len(story))  # Output: 77
 
-# Check if the story string ends with "goes" and print the result
-print(story.endswith("goes"))  # Output: True
+# # Check if the story string ends with "goes" and print the result
+# print(story.endswith("goes"))  # Output: True
 
-# Count the occurrences of the character 'c' in the story string and print the result
-print(story.count('c'))  # Output: 2
+# # Count the occurrences of the character 'c' in the story string and print the result
+# print(story.count('c'))  # Output: 2
 
-# Capitalize the first character of the story string and print the result
-print(story.capitalize())  # Output:  Once uopn a time Ritraj started Learnig python using code with Harry lets see how it goes 
+# # Capitalize the first character of the story string and print the result
+# print(story.capitalize())  # Output:  Once uopn a time Ritraj started Learnig python using code with Harry lets see how it goes 
 
-# Find the position of the substring "Ritu" in the story string and print the result
-print(story.find("Ritu"))  # Output: 20
+# # Find the position of the substring "Ritu" in the story string and print the result
+# print(story.find("Ritu"))  # Output: 20
 
-name = input("Enter your name: \n ")
-print("Good Afternoon " + name)
+# name = input("Enter your name: \n ")
+# print("Good Afternoon " + name)
 
 #List and tupples
 
-a= [1, 2, 3, 4, 5]
-b= [Apple, Banana, Mango, Orange]   
-print(a[0])  # Output: 1
-print(b[1])  # Output: Banana
-print(a[1:3])  # Output: [2, 3] 
-print(b[1:3])  # Output: [Banana, Mango]
+# a= [1, 2, 3, 4, 5]
+# b= ["Apple"," Banana", "Mango","Orange"]   
+# print(a[0])  # Output: 1
+# print(b[1])  # Output: Banana
+# print(a[1:3])  # Output: [2, 3] 
+# print(b[1:3])  # Output: [Banana, Mango]
+
+# # List Slicing 
+# Friends = ["Harry", "Rohan", "Hammad", "Shivam", "Shivani"]
+# print(Friends[0:3])  # Output: ['Harry', 'Rohan',
+
+# print(Friends[-4])  # Output: ['Hammad', 'Shivam', 'Shivani']
+
+#List Menthods 
+L1 = [2, 3, 5, 7, 11, 200, 17, 19]
+L2 = "Zaler"
+
+L1.sort() 
+print(L1)
+L1.reverse()
+print(L1)
+L1.append(100)  #
+print(L1)  # O
+L1.pop(100)  #
+print(L1)
+
+
+
+
+  
